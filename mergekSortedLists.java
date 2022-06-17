32890f77f1327d55ecbb533a3d73e169d261d09b
Christian Castro <46094200+gvmmybear@users.noreply.github.com>
2022-06-17 06:59:54 -0700

    Update mergekSortedLists.java
    cleaning up code

--- mergekSortedLists.java
+++ mergekSortedLists.java
@@ -8,20 +8,14 @@ class Solution {
         while(nodeNullCount < lists.length){
             for(int i = 0; i < lists.length; i++){
                 if(lists[i] != null){
-                    System.out.println(lists[i].val);
                     lists[i] = lists[i].next;
                 }else{
-                    // nodeNullCount += 1;
                     continue;
                 }
             }
             nodeNullCount += 1;
         }
-        
-        // System.out.println(lists[0].val);
-        // System.out.println(lists[0].next.val);
-        // System.out.println(lists[0].next.next.val);
-        
+    
         ListNode node = new ListNode();
         return node;
     }

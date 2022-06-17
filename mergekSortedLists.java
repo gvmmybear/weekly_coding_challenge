9a85736e3b59dc147b7104d57434947e548c7cd2
Christian Castro <46094200+gvmmybear@users.noreply.github.com>
2022-06-17 07:10:23 -0700

    Update mergekSortedLists.java
    accidentally uploading old code that wasn't working

--- mergekSortedLists.java
+++ mergekSortedLists.java
@@ -1,22 +1,38 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         ListNode myNode = new ListNode();
+        ListNode head = myNode;
         int minNodeIndex = 0;
         int nodeNullCount = 0;
-        int minNodeValue;
+        int minNodeValue = 0;
+        boolean hasMinVal = false;
        
         while(nodeNullCount < lists.length){
+            nodeNullCount = 0;
             for(int i = 0; i < lists.length; i++){
                 if(lists[i] != null){
-                    lists[i] = lists[i].next;
+                    if(!hasMinVal){
+                        hasMinVal = true;
+                        minNodeValue = lists[i].val;
+                        minNodeIndex = i;
+                    }else if(lists[i].val < minNodeValue){
+                        minNodeValue = lists[i].val;
+                        minNodeIndex = i;
+                    }
                 }else{
-                    continue;
+                    nodeNullCount += 1;
                 }
             }
-            nodeNullCount += 1;
+            if(nodeNullCount != lists.length){
+                myNode.val = minNodeValue;
+                myNode.next = nodeNullCount < lists.length - 1? new ListNode() : null;
+                myNode = myNode.next;
+                lists[minNodeIndex] = lists[minNodeIndex].next;
+                hasMinVal = false;
+                
+            }
         }
-    
         ListNode node = new ListNode();
-        return node;
+        return head;
     }
 }
